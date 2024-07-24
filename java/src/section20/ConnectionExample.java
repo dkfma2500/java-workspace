befe654aeddb7fd141048ae81870be608168954e
dkfma2500 <dkfma2500@naver.com>
2024-07-24 21:51:09 +0900

    JAVA수업

--- java/src/section20/ConnectionExample.java
+++ java/src/section20/ConnectionExample.java
@@ -1,7 +1,15 @@
 package section20;
 
+import java.sql.DriverManager;
+
 public class ConnectionExample {
     public static void main(String[] args) {
-        
+       // Connection conn = null;
+
+      //  try {
+       //     Class.forName("com.mysql,cj,jdbc.Driver");
+
+           // conn = DriverManager.getConnection(("jdbc:mysql://localhost/test?"+"user=minty&password=great"), null, null)
+        }
     }
-}
+
